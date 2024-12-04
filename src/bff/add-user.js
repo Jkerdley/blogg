ec248a05f377b2e48681321945f540c1b15ce88e
Jkerdley <jk-erley@yandex.ru>
2024-12-04 20:51:05 +0700

    создал BFF и отрефакторил код

---
+++ src/bff/add-user.js
@@ -0,0 +1,12 @@
+import { generateDate } from './generate-date';
+export const addUser = (login, password) =>
+	fetch('http://localhost:3005/users', {
+		method: 'POST',
+		headers: { 'Content-type': 'application/json;charset=utf-8' },
+		body: JSON.stringify({
+			login,
+			password,
+			regrole_id: 2,
+			registered_at: generateDate(),
+		}),
+	});
